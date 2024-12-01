a31355d32e07bed10c67b0c86259ee46aaae6c4d
imfighter003 <imfighter003@gmail.com>
2024-12-01 13:26:05 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,5 +1,4 @@
-api_id = 29410389
-api_hash = "0c716764715886f6641477ffbb63e1ee"
-bot_token = "5946934925:AAGoALZ3-Cr8hQaBdzHhmW3pknt-h1LVVpI"
-sudo_groups = -1001777454744
-
+api_id ="26993113"
+api_hash = "bea8bb154b07fc50ee3e308772f4366c"
+bot_token = "7568771148:AAGbVUMP40Y-M4eeiHO_CO7F0LXU-ybiYPc"
+sudo_groups =-1002285587854
